86275e9bc5a559c9325a9616957180712de89a2b
Marc-Antoine Ruel <maruel@chromium.org>
2016-01-10 19:46:35 -0500

    Fix build on go <=1.5.

--- internal/goversion1.6.go
+++ internal/goversion1.6.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 // +build go1.6
 
 package internal
