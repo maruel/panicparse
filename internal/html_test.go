maruel/panicparse

commit 8fe0a69c9dd8b4c82306843cc90ab9925f2dac6b

stack: Rename Bucketize to Aggregate, simplify Bucket
Change the returned slice to be a slice of pointers, which makes sorting
more efficient.

Simplify tests in bucket_test.go to be more focused.

internal/html.go
@@ -12,7 +12,7 @@ import (
 	"github.com/maruel/panicparse/stack"
 )
 
-func writeToHTML(html string, buckets []stack.Bucket, needsEnv bool) error {
+func writeToHTML(html string, buckets []*stack.Bucket, needsEnv bool) error {
 	m := template.FuncMap{
 		"funcClass":           funcClass,
 		"notoColorEmoji1F4A3": notoColorEmoji1F4A3,
@@ -27,7 +27,7 @@ func writeToHTML(html string, buckets []stack.Bucket, needsEnv bool) error {
 		return err
 	}
 	data := struct {
-		Buckets  []stack.Bucket
+		Buckets  []*stack.Bucket
 		Now      time.Time
 		NeedsEnv bool
 	}{buckets, time.Now().Truncate(time.Second), needsEnv}
@@ -59,7 +59,7 @@ func funcClass(line *stack.Call) template.HTML {
 }
 
 func routineClass(bucket *stack.Bucket) template.HTML {
-	if bucket.First() {
+	if bucket.First {
 		return "RoutineFirst"
 	}
 	return "Routine"

internal/main.go
@@ -55,13 +55,13 @@ var defaultPalette = Palette{
 	Arguments:          resetFG,
 }
 
-func writeToConsole(out io.Writer, p *Palette, buckets []stack.Bucket, fullPath, needsEnv bool, filter, match *regexp.Regexp) error {
+func writeToConsole(out io.Writer, p *Palette, buckets []*stack.Bucket, fullPath, needsEnv bool, filter, match *regexp.Regexp) error {
 	if needsEnv {
 		_, _ = io.WriteString(out, "\nTo see all goroutines, visit https://github.com/maruel/panicparse#gotraceback\n\n")
 	}
 	srcLen, pkgLen := CalcLengths(buckets, fullPath)
 	for _, bucket := range buckets {
-		header := p.BucketHeader(&bucket, fullPath, len(buckets) > 1)
+		header := p.BucketHeader(bucket, fullPath, len(buckets) > 1)
 		if filter != nil && filter.MatchString(header) {
 			continue
 		}
@@ -90,7 +90,7 @@ func process(in io.Reader, out io.Writer, p *Palette, s stack.Similarity, fullPa
 	if parse {
 		stack.Augment(c.Goroutines)
 	}
-	buckets := stack.Bucketize(c.Goroutines, s)
+	buckets := stack.Aggregate(c.Goroutines, s)
 	if html == "" {
 		return writeToConsole(out, p, buckets, fullPath, needsEnv, filter, match)
 	}

internal/ui.go
@@ -34,7 +34,7 @@ type Palette struct {
 }
 
 // CalcLengths returns the maximum length of the source lines and package names.
-func CalcLengths(buckets []stack.Bucket, fullPath bool) (int, int) {
+func CalcLengths(buckets []*stack.Bucket, fullPath bool) (int, int) {
 	srcLen := 0
 	pkgLen := 0
 	for _, bucket := range buckets {
@@ -75,7 +75,7 @@ func (p *Palette) functionColor(line *stack.Call) string {
 
 // routineColor returns the color for the header of the goroutines bucket.
 func (p *Palette) routineColor(bucket *stack.Bucket, multipleBuckets bool) string {
-	if bucket.First() && multipleBuckets {
+	if bucket.First && multipleBuckets {
 		return p.RoutineFirst
 	}
 	return p.Routine
@@ -95,7 +95,7 @@ func (p *Palette) BucketHeader(bucket *stack.Bucket, fullPath, multipleBuckets b
 	}
 	return fmt.Sprintf(
 		"%s%d: %s%s%s\n",
-		p.routineColor(bucket, multipleBuckets), len(bucket.Routines),
+		p.routineColor(bucket, multipleBuckets), len(bucket.IDs),
 		bucket.State, extra,
 		p.EOLReset)
 }

internal/ui_test.go
@@ -30,7 +30,7 @@ var testPalette = &Palette{
 }
 
 func TestCalcLengths(t *testing.T) {
-	b := []stack.Bucket{
+	b := []*stack.Bucket{
 		{
 			Signature: stack.Signature{
 				Stack: stack.Stack{
@@ -43,7 +43,8 @@ func TestCalcLengths(t *testing.T) {
 					},
 				},
 			},
-			Routines: nil,
+			IDs:   []int{},
+			First: true,
 		},
 	}
 	srcLen, pkgLen := CalcLengths(b, true)
@@ -72,12 +73,8 @@ func TestBucketHeader(t *testing.T) {
 			SleepMax: 6,
 			SleepMin: 2,
 		},
-		Routines: []stack.Goroutine{
-			{
-				First: true,
-			},
-			{},
-		},
+		IDs:   []int{1, 2},
+		First: true,
 	}
 	// When printing, it prints the remote path, not the transposed local path.
 	compareString(t, "B2: chan receive [2~6 minutes]D [Created by main.mainImpl @ /gopath/src/github.com/foo/bar/baz.go:74]A\n", testPalette.BucketHeader(b, true, true))
@@ -92,7 +89,8 @@ func TestBucketHeader(t *testing.T) {
 			SleepMin: 6,
 			Locked:   true,
 		},
-		Routines: nil,
+		IDs:   []int{},
+		First: true,
 	}
 	compareString(t, "C0: b0rked [6 minutes] [locked]A\n", testPalette.BucketHeader(b, false, false))
 }

stack/bucket.go
@@ -23,64 +23,66 @@ const (
 	AnyValue
 )
 
-// Bucketize merges similar goroutines into buckets.
+// Aggregate merges similar goroutines into buckets.
 //
-// The buckets are ordering in order of relevancy.
-func Bucketize(goroutines []Goroutine, similar Similarity) []Bucket {
-	b := map[*Signature][]Goroutine{}
+// The buckets are ordered in library provided order of relevancy. You can
+// reorder at your chosing.
+func Aggregate(goroutines []Goroutine, similar Similarity) []*Bucket {
+	type count struct {
+		ids   []int
+		first bool
+	}
+	b := map[*Signature]*count{}
 	// O(n²). Fix eventually.
 	for _, routine := range goroutines {
 		found := false
-		for key := range b {
+		for key, c := range b {
 			// When a match is found, this effectively drops the other goroutine ID.
 			if key.similar(&routine.Signature, similar) {
 				found = true
+				c.ids = append(c.ids, routine.ID)
+				c.first = c.first || routine.First
 				if !key.equal(&routine.Signature) {
 					// Almost but not quite equal. There's different pointers passed
 					// around but the same values. Zap out the different values.
 					newKey := key.merge(&routine.Signature)
-					b[newKey] = append(b[key], routine)
+					b[newKey] = c
 					delete(b, key)
-				} else {
-					b[key] = append(b[key], routine)
 				}
 				break
 			}
 		}
 		if !found {
+			// Create a copy of the Signature, since it will be mutated.
 			key := &Signature{}
 			*key = routine.Signature
-			b[key] = []Goroutine{routine}
+			b[key] = &count{ids: []int{routine.ID}, first: routine.First}
 		}
 	}
-	return sortBuckets(b)
+	out := make(buckets, 0, len(b))
+	for signature, c := range b {
+		sort.Ints(c.ids)
+		out = append(out, &Bucket{Signature: *signature, IDs: c.ids, First: c.first})
+	}
+	sort.Sort(out)
+	return out
 }
 
 // Bucket is a stack trace signature and the list of goroutines that fits this
 // signature.
 type Bucket struct {
 	Signature
-	Routines []Goroutine
-}
-
-// First returns true if it contains the first goroutine, e.g. the ones that
-// likely generated the panic() call, if any.
-func (b *Bucket) First() bool {
-	for _, r := range b.Routines {
-		if r.First {
-			return true
-		}
-	}
-	return false
+	// IDs is the ID of each Goroutine with this Signature.
+	IDs []int
+	// First is true if this Bucket contains the first goroutine, e.g. the one
+	// Signature that likely generated the panic() call, if any.
+	First bool
 }
 
 // less does reverse sort.
 func (b *Bucket) less(r *Bucket) bool {
-	if b.First() {
-		return true
-	}
-	if r.First() {
-		return false
+	if b.First || r.First {
+		return b.First
 	}
 	return b.Signature.less(&r.Signature)
 }
@@ -88,26 +90,16 @@ func (b *Bucket) less(r *Bucket) bool {
 //
 
 // buckets is a list of Bucket sorted by repeation count.
-type buckets []Bucket
+type buckets []*Bucket
 
 func (b buckets) Len() int {
 	return len(b)
 }
 
 func (b buckets) Less(i, j int) bool {
-	return b[i].less(&b[j])
+	return b[i].less(b[j])
 }
 
 func (b buckets) Swap(i, j int) {
 	b[j], b[i] = b[i], b[j]
-}
-
-// sortBuckets creates a list of Bucket from each goroutine stack trace count.
-func sortBuckets(b map[*Signature][]Goroutine) []Bucket {
-	out := make(buckets, 0, len(b))
-	for signature, count := range b {
-		out = append(out, Bucket{*signature, count})
-	}
-	sort.Sort(out)
-	return out
 }

stack/bucket_test.go
@@ -7,11 +7,12 @@ package stack
 import (
 	"bytes"
 	"io/ioutil"
+	"reflect"
 	"strings"
 	"testing"
 )
 
-func TestBucketizeNotAggressive(t *testing.T) {
+func TestAggregateNotAggressive(t *testing.T) {
 	// 2 goroutines with similar but not exact same signature.
 	data := []string{
 		"panic: runtime error: index out of range",
@@ -29,7 +30,8 @@ func TestBucketizeNotAggressive(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	expectedGR := []Goroutine{
+	actual := Aggregate(c.Goroutines, ExactLines)
+	expected := []*Bucket{
 		{
 			Signature: Signature{
 				State: "chan receive",
@@ -44,7 +46,7 @@ func TestBucketizeNotAggressive(t *testing.T) {
 					},
 				},
 			},
-			ID:    6,
+			IDs:   []int{6},
 			First: true,
 		},
 		{
@@ -61,18 +63,13 @@ func TestBucketizeNotAggressive(t *testing.T) {
 					},
 				},
 			},
-			ID: 7,
+			IDs: []int{7},
 		},
 	}
-	compareGoroutines(t, expectedGR, c.Goroutines)
-	expectedBuckets := []Bucket{
-		{expectedGR[0].Signature, []Goroutine{expectedGR[0]}},
-		{expectedGR[1].Signature, []Goroutine{expectedGR[1]}},
-	}
-	compareBuckets(t, expectedBuckets, Bucketize(c.Goroutines, ExactLines))
+	compareBuckets(t, expected, actual)
 }
 
-func TestBucketizeExactMatching(t *testing.T) {
+func TestAggregateExactMatching(t *testing.T) {
 	// 2 goroutines with the exact same signature.
 	data := []string{
 		"panic: runtime error: index out of range",
@@ -94,7 +91,8 @@ func TestBucketizeExactMatching(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	expectedGR := []Goroutine{
+	actual := Aggregate(c.Goroutines, ExactLines)
+	expected := []*Bucket{
 		{
 			Signature: Signature{
 				State: "chan receive",
@@ -113,36 +111,14 @@ func TestBucketizeExactMatching(t *testing.T) {
 					Func:    Func{Raw: "main.mainImpl"},
 				},
 			},
-			ID:    6,
+			IDs:   []int{6, 7},
 			First: true,
 		},
-		{
-			Signature: Signature{
-				State: "chan receive",
-				Stack: Stack{
-					Calls: []Call{
-						{
-							SrcPath: "/gopath/src/github.com/maruel/panicparse/stack/stack.go",
-							Line:    72,
-							Func:    Func{Raw: "main.func·001"},
-						},
-					},
-				},
-				CreatedBy: Call{
-					SrcPath: "/gopath/src/github.com/maruel/panicparse/stack/stack.go",
-					Line:    74,
-					Func:    Func{Raw: "main.mainImpl"},
-				},
-			},
-			ID: 7,
-		},
 	}
-	compareGoroutines(t, expectedGR, c.Goroutines)
-	expectedBuckets := []Bucket{{expectedGR[0].Signature, []Goroutine{expectedGR[0], expectedGR[1]}}}
-	compareBuckets(t, expectedBuckets, Bucketize(c.Goroutines, ExactLines))
+	compareBuckets(t, expected, actual)
 }
 
-func TestBucketizeAggressive(t *testing.T) {
+func TestAggregateAggressive(t *testing.T) {
 	// 3 goroutines with similar signatures.
 	data := []string{
 		"panic: runtime error: index out of range",
@@ -164,48 +140,12 @@ func TestBucketizeAggressive(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	expectedGR := []Goroutine{
+	actual := Aggregate(c.Goroutines, AnyPointer)
+	expected := []*Bucket{
 		{
 			Signature: Signature{
 				State:    "chan receive",
 				SleepMin: 10,
-				SleepMax: 10,
-				Stack: Stack{
-					Calls: []Call{
-						{
-							SrcPath: "/gopath/src/github.com/maruel/panicparse/stack/stack.go",
-							Line:    72,
-							Func:    Func{Raw: "main.func·001"},
-							Args:    Args{Values: []Arg{{Value: 0x11000000, Name: ""}, {Value: 2}}},
-						},
-					},
-				},
-			},
-			ID:    6,
-			First: true,
-		},
-		{
-			Signature: Signature{
-				State:    "chan receive",
-				SleepMin: 50,
-				SleepMax: 50,
-				Stack: Stack{
-					Calls: []Call{
-						{
-							SrcPath: "/gopath/src/github.com/maruel/panicparse/stack/stack.go",
-							Line:    72,
-							Func:    Func{Raw: "main.func·001"},
-							Args:    Args{Values: []Arg{{Value: 0x21000000, Name: "#1"}, {Value: 2}}},
-						},
-					},
-				},
-			},
-			ID: 7,
-		},
-		{
-			Signature: Signature{
-				State:    "chan receive",
-				SleepMin: 100,
 				SleepMax: 100,
 				Stack: Stack{
 					Calls: []Call{
@@ -213,30 +153,25 @@ func TestBucketizeAggressive(t *testing.T) {
 							SrcPath: "/gopath/src/github.com/maruel/panicparse/stack/stack.go",
 							Line:    72,
 							Func:    Func{Raw: "main.func·001"},
-							Args:    Args{Values: []Arg{{Value: 0x21000000, Name: "#1"}, {Value: 2}}},
+							Args:    Args{Values: []Arg{{Value: 0x11000000, Name: "*"}, {Value: 2}}},
 						},
 					},
 				},
 			},
-			ID: 8,
+			IDs:   []int{6, 7, 8},
+			First: true,
 		},
 	}
-	compareGoroutines(t, expectedGR, c.Goroutines)
-	signature := Signature{
-		State:    "chan receive",
-		SleepMin: 10,
-		SleepMax: 100,
-		Stack: Stack{
-			Calls: []Call{
-				{
-					SrcPath: "/gopath/src/github.com/maruel/panicparse/stack/stack.go",
-					Line:    72,
-					Func:    Func{Raw: "main.func·001"},
-					Args:    Args{Values: []Arg{{Value: 0x11000000, Name: "*"}, {Value: 2}}},
-				},
-			},
-		},
+	compareBuckets(t, expected, actual)
+}
+
+func compareBuckets(t *testing.T, expected, actual []*Bucket) {
+	if len(expected) != len(actual) {
+		t.Fatalf("Different []Bucket length:\n- %v\n- %v", expected, actual)
 	}
-	expectedBuckets := []Bucket{{signature, []Goroutine{expectedGR[0], expectedGR[1], expectedGR[2]}}}
-	compareBuckets(t, expectedBuckets, Bucketize(c.Goroutines, AnyPointer))
+	for i := range expected {
+		if !reflect.DeepEqual(expected[i], actual[i]) {
+			t.Fatalf("Different Bucket:\n- %#v\n- %#v", expected[i], actual[i])
+		}
+	}
 }

stack/context_test.go
@@ -38,7 +38,7 @@ func Example() {
 	}
 
 	// Find out similar goroutine traces and group them into buckets.
-	buckets := Bucketize(c.Goroutines, AnyValue)
+	buckets := Aggregate(c.Goroutines, AnyValue)
 
 	// Calculate alignment.
 	srcLen := 0
@@ -66,7 +66,7 @@ func Example() {
 		if c := bucket.CreatedByString(false); c != "" {
 			extra += " [Created by " + c + "]"
 		}
-		fmt.Printf("%d: %s%s\n", len(bucket.Routines), bucket.State, extra)
+		fmt.Printf("%d: %s%s\n", len(bucket.IDs), bucket.State, extra)
 
 		// Print the stack lines.
 		for _, line := range bucket.Stack.Calls {
@@ -740,17 +740,6 @@ func compareGoroutines(t *testing.T, expected, actual []Goroutine) {
 	for i := range expected {
 		if !reflect.DeepEqual(expected[i], actual[i]) {
 			t.Fatalf("Different Goroutine:\n- %v\n- %v", expected[i], actual[i])
-		}
-	}
-}
-
-func compareBuckets(t *testing.T, expected, actual []Bucket) {
-	if len(expected) != len(actual) {
-		t.Fatalf("Different []Bucket length:\n- %v\n- %v", expected, actual)
-	}
-	for i := range expected {
-		if !reflect.DeepEqual(expected[i], actual[i]) {
-			t.Fatalf("Different Bucket:\n- %#v\n- %#v", expected[i], actual[i])
 		}
 	}
 }

commit 679138e3406aa5f332dc646f817743ad684b8f40

Fix html regression in 8fe0a69c9dd8b4
Add simple unit test to stop regressing.

internal/html.go
@@ -113,7 +113,7 @@ href=https://github.com/maruel/panicparse#gotraceback>github.com/maruel/panicpar
 <div id="content">
 {{range .Buckets}}
 	<h1>{{if .First}}Panicking {{end}}Routine</h1>
-	<span class="{{routineClass .}}">{{len .Routines}}: <span class="state">{{.State}}</span>
+	<span class="{{routineClass .}}">{{len .IDs}}: <span class="state">{{.State}}</span>
 	{{if .SleepMax -}}
 	  {{- if ne .SleepMin .SleepMax}} <span class="sleep">[{{.SleepMin}}~{{.SleepMax}} minutes]</span>
 		{{- else}} <span class="sleep">[{{.SleepMax}} minutes]</span>

internal/html_test.go
@@ -1,0 +1,52 @@
+// Copyright 2018 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package internal
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/maruel/panicparse/stack"
+)
+
+func TestWriteToHTML(t *testing.T) {
+	f, err := ioutil.TempFile("", "panicparse")
+	if err != nil {
+		t.Fatal(err)
+	}
+	n := f.Name()
+	f.Close()
+	defer func() {
+		if err := os.Remove(n); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	buckets := []*stack.Bucket{
+		{
+			Signature: stack.Signature{
+				State: "chan receive",
+				Stack: stack.Stack{
+					Calls: []stack.Call{
+						{
+							SrcPath: "/gopath/src/github.com/maruel/panicparse/stack/stack.go",
+							Line:    72,
+							Func:    stack.Func{Raw: "main.func·001"},
+							Args:    stack.Args{Values: []stack.Arg{{Value: 0x11000000, Name: ""}, {Value: 2}}},
+						},
+					},
+				},
+			},
+			IDs:   []int{1, 2},
+			First: true,
+		},
+		{
+			IDs: []int{3},
+		},
+	}
+	if err := writeToHTML(n, buckets, true); err != nil {
+		t.Fatal(err)
+	}
+}