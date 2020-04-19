403fe2626acee2e4c22568bfe178eed62b3a3e2f
Marc-Antoine Ruel <maruel@chromium.org>
2020-04-19 19:15:24 -0400

    travis: Add license check
    Fix two files
    
    Remove old hack now unnecessary for go test ./... with code coverage.
    
    Make sure misspell is not run on vendor/.
    
    Clean up cache entry.
    
    Fix alignment.

--- cmd/panic/internal/incorrect/correct.go
+++ cmd/panic/internal/incorrect/correct.go
@@ -1,3 +1,7 @@
+// Copyright 2020 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 // Package correct is in directory incorrect. If the call stack is
 // incorrect.Panic(), you know the parsing failed.
 package correct
--- cmd/panic/internal/ùtf8/ùtf8.go
+++ cmd/panic/internal/ùtf8/ùtf8.go
@@ -1,3 +1,7 @@
+// Copyright 2020 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 // Package ùtf8 tests a package and function with non-ASCII names.
 package ùtf8
 
