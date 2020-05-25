0c07b973a7730986389708bf1336bc18140e330d
Marc-Antoine Ruel <maruel@chromium.org>
2020-05-25 16:20:45 -0400

    stack: fetch stringer before calling it
    This will enable checking go generate on the CI.

--- stack/context.go
+++ stack/context.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+//go:generate go get golang.org/x/tools/cmd/stringer
 //go:generate stringer -type state
 
 package stack
