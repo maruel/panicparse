1a18ef8d122c4880486db048a25dc8bf789ec92c
Jaroslav Rakhmatoullin <jazzoslav@gmail.com>
2020-12-11 16:24:38 +0100

    terminal: handle ctrl+c

--- main.go
+++ main.go
@@ -11,13 +11,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/maruel/panicparse/v2/internal"
 )
 
 func main() {
+	SetupCloseHandler()
 	if err := internal.Main(); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+func SetupCloseHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		os.Exit(0)
+	}()
 }
