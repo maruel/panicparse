7d1abed4bb62ca94ab42b0f9df16210c25ae38f7
Marc-Antoine Ruel <maruel@gmail.com>
2024-11-28 19:11:14 -0500

    Unbreak go generate

--- stack/context.go
+++ stack/context.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-//go:generate go install golang.org/x/tools/cmd/stringer@latest
+//go:generate go install golang.org/x/tools/cmd/stringer@v0.24.0
 //go:generate stringer -type state
 //go:generate stringer -type Location
 
